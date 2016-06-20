8f9659b897a1b3f608ffa19e05cc1b5d2fa4fdf5
xiaodong <xiaodong.li@asiainnovations.com>
2016-06-20 17:40:28 +0800

    update

--- es6/Generator.js
+++ es6/Generator.js
@@ -153,4 +153,43 @@ co(gen);
 co(gen).then(function (){
     console.log('Generator 函数执行完成');
 });
-//上面代码中，等到Generator函数执行结束，就会输出一行提示。
+//上面代码中，等到Generator函数执行结束，就会输出一行提示
+//
+
+
+function co(gen){
+    var ctx = this;
+
+    return new Promise(function (resolve, reject) {
+        if(typeof gen === 'function'){
+            gen = gen.call(ctx);
+        }
+        if(!gen || typeof gen.next !== 'function'){
+            return resolve(gen);
+        }
+
+        onFulfilled();
+
+        function onFulfilled(res){
+            var ret;
+            try{
+                ret = gen.next(res);
+            } catch (e) {
+                return reject(e)
+            }
+
+            next(ret);
+        }
+
+        function next(ret) {
+            if (ret.done) return resolve(ret.value);
+            var value = toPromise.call(ctx, ret.value);
+            if (value && isPromise(value)) return value.then(onFulfilled, onRejected);
+            return onRejected(new TypeError('You may only yield a function, promise, generator, array, or object, '
+                + 'but the following object was passed: "' + String(ret.value) + '"'));
+        }
+
+    });
+}
+
+//async函数
