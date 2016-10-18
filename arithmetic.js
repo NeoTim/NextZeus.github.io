1fd5acafbdc0c14f02e084f4e0c9e775ea409f49
小东 <NextZeus@users.noreply.github.com>
2016-10-18 22:04:07 +0800

    Update arithmetic.js

--- arithmetic.js
+++ arithmetic.js
@@ -37,3 +37,23 @@ function selectionSort(arr) {
 }
 var arr=[3,44,38,5,47,15,36,26,27,2,46,4,19,50,48];
 console.log(selectionSort(arr));
+
+//插入排序
+function insertionSort(array) {
+    if (Object.prototype.toString.call(array).slice(8, -1) === 'Array') {
+        console.time('插入排序耗时：');
+        for (var i = 1; i < array.length; i++) {
+            var key = array[i];
+            var j = i - 1;
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
+        console.timeEnd('插入排序耗时：');
+        return array;
+    } else {
+        return 'array is not an Array!';
+    }
+}
