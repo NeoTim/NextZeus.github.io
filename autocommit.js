cb0f179b6c87abca28ea49b62d06415d6da1d0bf
xiaodong <875697126@qq.com>
2017-07-12 12:24:50 +0800

    auto commit

--- autocommit.js
+++ autocommit.js
@@ -1,24 +1,32 @@
 var shell = require("shelljs");
 var fs = require("fs");
 var cmd = process.cwd();
-var file = cmd + "/autocommit.txt";
-fs.appendFileSync(file,'updateTime: ' + Date.now()+';\n');
 
-var command = "cd " + cmd;
-console.log("command ",command);
-shell.exec(command);
+function auto(){
+    var file = cmd + "/autocommit.txt";
+    fs.appendFileSync(file,'updateTime: ' + Date.now()+';\n');
 
-command = "sh modified_git.sh";
-console.log("command ",command);
-shell.exec(command);
+    var command = "cd " + cmd;
+    console.log("command ",command);
+    shell.exec(command);
 
-command = "git status";
-console.log("command ",command);
-shell.exec(command);
+    command = "sh modified_git.sh";
+    console.log("command ",command);
+    shell.exec(command);
 
-command = "git commit -a -m 'auto commit'";
-shell.exec(command);
+    command = "git status";
+    console.log("command ",command);
+    shell.exec(command);
 
-command = "git push";
-shell.exec(command);
+    command = "git commit -a -m 'auto commit'";
+    shell.exec(command);
+
+    command = "git push";
+    shell.exec(command);
+    console.log('done!!!');
+}
+
+setInterval(function(){
+    auto();
+},1 * 60 * 1000);
 
